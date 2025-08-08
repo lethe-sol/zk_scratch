c1ee7abadeef4d6298d450256dc2b82c8eb598ad
lethe-sol <arjy2005@gmail.com>
2025-08-07 22:13:59 -0400

    Update mod.rs

--- programs/tornado_mixer/src/instructions/mod.rs
+++ programs/tornado_mixer/src/instructions/mod.rs
@@ -2,6 +2,7 @@ pub mod initialize;
 pub mod deposit;
 pub mod withdraw;
 
+// Re-export only the handler functions
 pub use initialize::initialize;
 pub use deposit::deposit;
 pub use withdraw::withdraw;
