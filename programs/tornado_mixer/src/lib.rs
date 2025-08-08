65147e8fe9bed10c5bd28c754cdf4d822272344c
lethe-sol <arjy2005@gmail.com>
2025-08-07 22:21:31 -0400

    Update lib.rs

--- programs/tornado_mixer/src/lib.rs
+++ programs/tornado_mixer/src/lib.rs
@@ -14,26 +14,26 @@ pub mod tornado_mixer {
     use anchor_lang::prelude::Result; // Anchor's Result alias
 
     pub fn initialize(
-        ctx: Context<instructions::Initialize>,
+        ctx: Context<instructions::initialize::Initialize>,
         deposit_amount: u64,
     ) -> Result<()> {
-        instructions::initialize(ctx, deposit_amount)
+        instructions::initialize::initialize(ctx, deposit_amount)
     }
 
     pub fn deposit(
-        ctx: Context<instructions::Deposit>,
+        ctx: Context<instructions::deposit::Deposit>,
         commitment: [u8; 32],
     ) -> Result<()> {
-        instructions::deposit(ctx, commitment)
+        instructions::deposit::deposit(ctx, commitment)
     }
 
     pub fn withdraw(
-        ctx: Context<instructions::Withdraw>,
+        ctx: Context<instructions::withdraw::Withdraw>,
         proof: [u8; 256],
         root: [u8; 32],
         nullifier_hash: [u8; 32],
         recipient: Pubkey,
     ) -> Result<()> {
-        instructions::withdraw(ctx, proof, root, nullifier_hash, recipient)
+        instructions::withdraw::withdraw(ctx, proof, root, nullifier_hash, recipient)
     }
 }
