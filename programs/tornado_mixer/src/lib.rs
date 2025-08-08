846eaa6ed93c8009c714905aadf32a71690ee6f9
lethe-sol <arjy2005@gmail.com>
2025-08-07 22:26:22 -0400

    Update lib.rs

--- programs/tornado_mixer/src/lib.rs
+++ programs/tornado_mixer/src/lib.rs
@@ -4,7 +4,7 @@ declare_id!("31zAuv25vz5tV8oiHuq49Zd827mNpbaaZ6P7da6hHB8g");
 
 pub mod constants;
 pub mod errors;
-pub mod instructions; // MUST be public so Anchor can find it
+pub mod instructions; // must be public for Anchor's macro to find it
 pub mod state;
 pub mod verifying_key;
 
